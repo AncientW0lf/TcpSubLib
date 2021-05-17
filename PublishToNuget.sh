41a0ca05982c2e90f45401abde7ba03852fcf45b
LunarDiver <25Prozent@gmail.com>
2021-05-17 08:09:33 +0200

    Fixed PublishToNuget cleaning debug folder.

--- PublishToNuget.sh
+++ PublishToNuget.sh
@@ -2,6 +2,6 @@
 echo Enter NuGet key:
 read key
 
-dotnet clean
+dotnet clean -c Release
 dotnet build -c Release
 dotnet nuget push ./TcpSubLib/bin/Release/*.nupkg -k $key -s https://api.nuget.org/v3/index.json
